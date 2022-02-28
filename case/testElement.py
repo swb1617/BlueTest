3a7eabbc746a8fe5f300423f3aa6e916269ad00c
shiwenbin <1503593264@qq.com>
2022-02-28 16:55:32 +0800

    线路规划

--- case/testElement.py
+++ case/testElement.py
@@ -3,15 +3,18 @@ user:石文斌
 time：2022/01/05
 
 """
+import logging
 import time
 import unittest
 
 from appium import webdriver
 
 from config import desired_caps, max_number, userPassword, userID
+from element.button import Tap
 from element.elementLogin import Login
 from common.tool import Tool
 from common.retryTest import retry
+from element.elementUI import UI
 
 
 class TestLogin(unittest.TestCase):
@@ -19,11 +22,20 @@ class TestLogin(unittest.TestCase):
     def setUp(self) -> None:  # 执行方法前准备工作
         self.parme = {"port": 4723}
         self.driver = webdriver.Remote('http://localhost:%s/wd/hub' % str(self.parme['port']), desired_caps)
-        Tool.InstallApp(self)
+        # Tool.InstallApp(self)
         Tool.judge_app_state(self)
         self.driver.implicitly_wait(20)  # 稳定元素
 
     def tearDown(self) -> None:  # 执行方法后工作
         time.sleep(3)
 
+    def testIntelligentNotification(self):
+        Tap.GetToDevice(self).click()
+        time.sleep(3)
+        UI.GetDevicesIntelligentNotificationInfo(self).click()
+        time.sleep(3)
+        UI.GetDevicesIntelligentNotificationBack(self).click()
 
+    def test_1(self):
+        print(1)
+        logging.ERROR(print(1))
